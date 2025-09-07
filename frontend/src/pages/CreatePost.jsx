dcf8d5d73de25fef16e6848926f5ac54ea4e64d5
sairam <sairampolisetty0@gmail.com>
2025-09-07 17:49:11 +0530

    changed frontend url

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ const CreatePost = () => {
       if(form.prompt){
         try{
           setGeneratingImg(true);
-          const response=await fetch('http://localhost:8080/api/v1/imaginezy',{
+          const response=await fetch('https://imaginezy.onrender.com/api/v1/imaginezy',{
             method:'POST',
             headers:{
               'Content-Type':'application/json',
@@ -44,7 +44,7 @@ const CreatePost = () => {
     if(form.prompt && form.photo){
       setLoading(true);
       try{
-        const response = await fetch('http://localhost:8080/api/v1/post',{
+        const response = await fetch('https://imaginezy.onrender.com/api/v1/post',{
           method:'POST',
           headers:{
             'Content-Type':'application/json',
