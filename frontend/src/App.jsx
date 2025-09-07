f86015b8105cc2e48c0c6c0f9e727b9c038a564f
krishna-Prasad-CEO <krishnaprasadandco12@gmail.com>
2025-09-07 18:40:32 +0530

    added authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,45 @@
-import React from 'react'
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
-import {logo} from './assets'
-import {Home, CreatePost} from './pages'
+import { logo } from "./assets";
+import { Home, CreatePost } from "./pages";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to="/">
-            <img src={logo} className="w-28 object-contain" alt="logo"/>
+          <img src={logo} className="w-28 object-contain" alt="logo" />
         </Link>
-        <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
+        <div className="flex justify-between">
+          <Link
+            to="/create-post"
+            className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md me-5"
+          >
             Create
-        </Link>
-    </header>
-    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+          </Link>
+          <SignedOut>
+            <SignInButton className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md" />
+          </SignedOut>
+          <SignedIn>
+            <UserButton className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md" />
+          </SignedIn>
+        </div>
+      </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
         </Routes>
-    </main>
+      </main>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
